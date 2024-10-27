b01460a307e1ba3acda1d12a19e59d511883d084
Guilherme Araujo <guidrop17@gmail.com>
2024-10-27 19:57:20 -0300

    Feat: Initial Commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Nathália Mendes dos Santos on 27/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
